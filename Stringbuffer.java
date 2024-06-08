f021637ffe092ab2a3a7568a3a7235780ced9f22
mouni9014 <jangitimounika9014@gmail.com>
2024-06-09 01:56:35 +0530

    Add files via upload

---
+++ Stringbuffer.java
@@ -0,0 +1,16 @@
+public class Stringbuffer {
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer("mounika");
+        
+        //System.out.println(sb.capacity());
+        //System.out.println(sb.length());
+         sb.append("ammu");
+
+         sb.deleteCharAt(2);
+         System.out.println(sb); 
+         
+     
+    }
+    
+}
---
+++ string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String[] args) {
+        String name = new String("mounika");// String name = "mounika";
+        System.out.println("hello" + name);
+        System.out.println(name);
+        System.out.println(name.charAt(4));
+    }
+    
+}
