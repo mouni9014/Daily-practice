672c050a103ef979c5f590f1830c6b71b827060b
mouni9014 <jangitimounika9014@gmail.com>
2024-06-09 17:14:24 +0530

    Add files via upload

---
+++ Constructors.java
@@ -0,0 +1,53 @@
+ class Person{
+    private String name;
+    private int age;
+
+    public Person(){ // default constructor
+        name = "ammu";
+        age = 22;
+    }
+
+    public Person(String name,int age){ //Parameteriazed Constructor
+        this.name = name;
+        this.age = age;
+
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        if(age > 0){
+            this.age= age;
+        }
+
+    }
+    public void displayDetails(){
+        System.out.println("Name: " + name + "Age: " + age);
+    }
+}
+public class Constructors {
+    public static void main(String[] args) {
+       
+        Person obj = new Person("mounika", 23);
+
+
+    
+    
+    
+
+        System.out.println(obj.getName());
+        System.out.println(obj.getAge());
+
+        obj.setName("Ammulu");
+        obj.setAge(23);
+        obj.displayDetails();
+    }
+    
+}
